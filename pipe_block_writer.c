5d0ee0effcde7280c7653a9cf0cac8c9895224bd
楠 吴 <wunan8312@outlook.com>
2018-11-26 20:40:18 +0800

    fix pipe issue

--- pipe_block_writer.c
+++ pipe_block_writer.c
@@ -10,25 +10,30 @@ int main()
     char c = 'c';
 
     if (pipe(p) == -1)
-	exit(1);
+        exit(1);
 
-    if (fcntl(p[1], F_SETFL, O_NONBLOCK) < 0) {
-	exit(-3);
+    if (fcntl(p[1], F_SETFL, O_NONBLOCK) < 0)
+    {
+        exit(-3);
     }
-    while (1) {
+    while (1)
+    {
 
-	if (write(p[1], &c, 1) == -1) {
-	    perror("write");
-	    break;
-	}
-	++cnt;
+        if (write(p[1], &c, 1) == -1)
+        {
+            perror("write");
+            break;
+        }
+        ++cnt;
     }
     printf("the size of pipe is %d\n", cnt);
-    if (close(p[0]) == -1) {
-	perror("close p[0]");
+    if (close(p[0]) == -1)
+    {
+        perror("close p[0]");
     }
-    if (close(p[1]) == -1) {
-	perror("close p[1]");
+    if (close(p[1]) == -1)
+    {
+        perror("close p[1]");
     }
     return 0;
 }
--- pipe_sync.c
+++ pipe_sync.c
@@ -1,58 +1,58 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<string.h>
-#include<sys/stat.h>
-#include<sys/types.h>
-#include<sys/wait.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[])
 {
-    int pfd[2];
-    int j, dummy;
+	int pfd[2];
+	int j, dummy;
 
-    if (argc < 2 || strcmp(argv[1], "--help") == 0)
-	perror("sleep-time...\n");
+	setbuf(stdout, NULL);
 
-    setbuf(stdout, NULL);
-
-    if (pipe(pfd) == -1)
-	perror("pipe");
-
-    for (j = 0; j < 2; j++) {
-	switch (fork()) {
-	case -1:
-	    perror("fork");
-	case 0:
-	    if (close(pfd[0]) == -1)
-		perror("close");
-
-
-	    sleep(3);
-	    printf("Child %d (PID=%ld) closing pipe\n", j,
-		   (long) getpid());
-	    if (close(pfd[1]) == -1)
-		perror("close");
-
-
-	    _exit(EXIT_SUCCESS);
-
-	default:
-	    break;
+	if (pipe(pfd) == -1)
+	{
+		perror("pipe");
 	}
-    }
 
+	for (j = 0; j < 2; j++)
+	{
+		switch (fork())
+		{
+		case -1:
+			perror("fork");
+		case 0:
+			if (close(pfd[0]) == -1)
+			{
+				perror("close");
+			}
 
-    if (close(pfd[1]) == -1)
-	perror("close");
+			sleep(j + 1);
 
+			printf("Child %d (PID=%ld) closing pipe\n", j,
+				   (long)getpid());
+			if (close(pfd[1]) == -1)
+			{
+				perror("close");
+			}
 
-    if (read(pfd[0], &dummy, 1) != 0)
-	perror("parent didn't get EOF");
+			_exit(EXIT_SUCCESS);
 
-    printf("  Parent ready to go\n");
+		default:
+			break;
+		}
+	}
 
+	if (close(pfd[1]) == -1)
+		perror("close");
 
-    exit(EXIT_SUCCESS);
+	if (read(pfd[0], &dummy, 1) != 0)
+		perror("parent didn't get EOF");
+
+	printf("  Parent ready to go\n");
+
+	exit(EXIT_SUCCESS);
 }
