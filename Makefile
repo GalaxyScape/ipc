03ea21c60e3c91ece3d9fdf96b378e2009427b7c
楠 吴 <wunan8312@outlook.com>
2018-11-26 21:07:38 +0800

    add Makefile and format pipe.c

---
+++ Makefile
@@ -0,0 +1,22 @@
+all: message_queue pipe_block_writer pipe pipe_sync
+
+message_queue: message_queue.c
+	clang -o message_queue message_queue.c -Wall -pthread -lrt
+
+pipe_block_writer: pipe_block_writer.c.c
+	clang -o pipe_block_writer.c pipe_block_writer.c -Wall -pthread -lrt
+
+pipe: pipe.c
+	clang -o pipe pipe.c -Wall -pthread -lrt
+
+pipe_sync: pipe_sync.c
+	clang -o pipe_sync pipe_sync.c -Wall -pthread -lrt
+
+shm_cli: shm_cli.c
+	clang -o shm_cli shm_cli.c -Wall -pthread -lrt
+
+shm_ser: shm_ser.c
+	clang -o shm_ser shm_ser.c -Wall -pthread -lrt
+
+#program2: program2.c
+#	clang -o program2 program2.c
--- pipe.c
+++ pipe.c
@@ -7,12 +7,11 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #define MAXMSG 100
-int
-main()
+int main()
 {
-  char msg[][MAXMSG] = { "This is an awesome attempt to go far "
-                         "Go go go this is the light of the dawn",
-                         "The Hobbit is something amazing" };
+  char msg[][MAXMSG] = {"This is an awesome attempt to go far "
+                        "Go go go this is the light of the dawn",
+                        "The Hobbit is something amazing"};
   sem_t sem;
   sem_init(&sem, 0, 1);
   // pipe
@@ -22,31 +21,33 @@ main()
   if (pipe(pfd) == -1)
     perror("pipe");
 
-  for (int i = 0; i < 3; ++i) {
-    switch (fork()) {
-      case -1:
-        perror("Fork Error\n");
-      case 0: // child
-        sem_wait(&sem);
-        
-	// generate messages
+  for (int i = 0; i < 3; ++i)
+  {
+    switch (fork())
+    {
+    case -1:
+      perror("Fork Error\n");
+    case 0: // child
+      sem_wait(&sem);
 
-        if (close(pfd[0]) == -1) // read end not used
-          perror("close");
-        write(pfd[1], msg[i], MAXMSG);
-        sem_post(&sem);
-        _exit(EXIT_SUCCESS);
-      default:
-        break;
+      // generate messages
+
+      if (close(pfd[0]) == -1) // read end not used
+        perror("close");
+      write(pfd[1], msg[i], MAXMSG);
+      sem_post(&sem);
+      _exit(EXIT_SUCCESS);
+    default:
+      break;
     }
   }
-
 
   printf("Father\n");
   char buffer[MAXMSG];
   if (close(pfd[1]) == -1) /* Write end is unused */
     perror("close");
-  for (int i = 0; i < 3; ++i) {
+  for (int i = 0; i < 3; ++i)
+  {
     printf("%d\n", wait(NULL));
     read(pfd[0], buffer, MAXMSG);
     printf("%s\n", buffer);
