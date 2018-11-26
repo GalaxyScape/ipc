ff915ba4d60c9c39f94d6ce88ef6085d098785ff
楠 吴 <wunan8312@outlook.com>
2018-11-26 21:27:05 +0800

    shm

--- shm_cli.c
+++ shm_cli.c
@@ -2,10 +2,10 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <fcntl.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
@@ -19,10 +19,10 @@ int main(int argc, char *argv[])
 
     sem_x = sem_open("read_sync", O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO, 0);
     sem_y =
-	sem_open("write_sync", O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO, 0);
+        sem_open("write_sync", O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO, 0);
 
     if (sem_x == SEM_FAILED || sem_y == SEM_FAILED)
-	perror("sem_open");
+        perror("sem_open");
 
     //begin
     int fd;
@@ -37,50 +37,61 @@ int main(int argc, char *argv[])
     umask(old_umask);
 
     if (fd == -1)
-	{
+    {
         perror("shm_open");
         exit(-1)
     }
-    char arr[100] = { };
+    char arr[100] = {};
     scanf("%s", arr);
     len = strlen(arr) + 1;
-    if (ftruncate(fd, len) == -1) {
-	perror("ftruncate");
+    if (ftruncate(fd, len) == -1)
+    {
+        perror("ftruncate");
+        exit(-1);
     }
-    printf("Resized to %ld bytes\n", (long) len);
-
+    printf("Resized to %ld bytes\n", (long)len);
 
     struct stat sb;
 
-    if (fstat(fd, &sb) == -1) {
-	perror("stat");
-	exit(-1);
+    if (fstat(fd, &sb) == -1)
+    {
+        perror("stat");
+        exit(-1);
     }
 
     printf("sb.st_size is %ld\n", sb.st_size);
 
     addr = mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (addr == MAP_FAILED)
-	perror("mmap");
+    {
+        perror("mmap");
+        exit(-1);
+    }
 
     memcpy(addr, arr, len);
     //end
 
-
-    if (sem_post(sem_x) == -1) {
-	perror("sem_post");
+    if (sem_post(sem_x) == -1)
+    {
+        perror("sem_post");
+        exit(-1);
     }
     printf("After sem_x receiver to go\n");
 
-    if (sem_wait(sem_y) == -1) {
-	perror("sem_wait");
+    if (sem_wait(sem_y) == -1)
+    {
+        perror("sem_wait");
+        exit(-1);
     }
     //begin
     memcpy(arr, addr, 5);
     printf("%s from writer\n", arr);
     //end
     if (close(fd) == -1)
-	perror("close");
+    {
+        perror("close");
+        exit(-1);
+    }
 
     sem_close(sem_x);
     sem_close(sem_y);
--- shm_ser.c
+++ shm_ser.c
@@ -1,10 +1,10 @@
 #include <semaphore.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/mman.h>
 
 int main(int argc, char *argv[])
@@ -26,12 +26,15 @@ int main(int argc, char *argv[])
 
     sem_x = sem_open("read_sync", O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO, 0);
     sem_y =
-	sem_open("write_sync", O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO, 0);
+        sem_open("write_sync", O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO, 0);
     if (sem_x == SEM_FAILED || sem_y == SEM_FAILED)
-	perror("sem_open");
+        perror("sem_open");
+    exit(-1);
 
-    if (sem_wait(sem_x) == -1) {
-	perror("sem_wait");
+    if (sem_wait(sem_x) == -1)
+    {
+        perror("sem_wait");
+        exit(-1);
     }
     //begin
     mode_t old_umask = umask(0);
@@ -41,43 +44,55 @@ int main(int argc, char *argv[])
     // restore old
     umask(old_umask);
     if (fd == -1)
-	{
+    {
         perror("shm_open");
         exit(-1);
     }
     printf("fd  %d\n", fd);
     char arr[100];
-    if (fstat(fd, &sb) == -1) {
-	perror("stat");
-	exit(-1);
+    if (fstat(fd, &sb) == -1)
+    {
+        perror("stat");
+        exit(-1);
     }
-    len = sb.st_size;		
+    len = sb.st_size;
     printf("sb.st_size is %d\n", len);
     addr = mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (addr == MAP_FAILED)
-	perror("mmap");
-
+    {
+        perror("mmap");
+        exit(-1);
+    }
 
     memcpy(arr, addr, len);
     printf("Here is what I get\n");
     printf("%s\n", arr);
 
-    if (len < strlen("over") - 1) {
-	if (ftruncate(fd, len + 1) == -1)
-	    perror("ftruncate");
-	printf("Resized to %ld bytes\n", (long) strlen("over"));
+    if (len < strlen("over") - 1)
+    {
+        if (ftruncate(fd, len + 1) == -1)
+        {
+            perror("ftruncate");
+            exit(-1);
+        }
 
+        printf("Resized to %ld bytes\n", (long)strlen("over"));
     }
     char str_v[] = "over";
     memcpy(addr, str_v, strlen(str_v) + 1);
     //end
     printf("before post sem_y\n");
-    if (sem_post(sem_y) == -1) {
-	perror("sem_post");
+    if (sem_post(sem_y) == -1)
+    {
+        perror("sem_post");
+        exit(-1);
     }
 
     if (close(fd) == -1)
-	perror("close");
+    {
+        perror("close");
+        exit(-1);
+    }
 
     sem_close(sem_x);
     sem_close(sem_y);
