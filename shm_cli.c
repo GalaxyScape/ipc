a8d0faa963fa584562e197a115de67b286c53902
楠 吴 <wunan8312@outlook.com>
2018-11-26 21:21:51 +0800

    Update shm_cli.c

--- shm_cli.c
+++ shm_cli.c
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
 	exit(-1);
     }
 
-    printf("sb.st_size is %d\n", sb.st_size);
+    printf("sb.st_size is %ld\n", sb.st_size);
 
     addr = mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (addr == MAP_FAILED)
